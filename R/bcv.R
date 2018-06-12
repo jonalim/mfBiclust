0242723e16c221114282ebcf944823aced20bfa8
jonalim <jlim7@comcast.net>
2018-06-12 16:18:32 +0100

    Rewrite esaBcv wrappers to recover from errors silently
    Former-commit-id: a66cb62142a4e0bea7febd0bf2176ff39696d569

--- R/bcv.R
+++ R/bcv.R
@@ -1,27 +1,23 @@
 # Use holdoutRepeat = NULL to do as many folds as max(dim(m) ./ holdouts) aka
 # k-fold but with random (not indexed) fold sampling each time. Known from esaBcv v1.2.1 source code.
-rcvs <- function(m, maxPCs, holdoutRepeat = 100, niter = 1, center = FALSE) {
-  # EsaBcv(r.limit = 20, nRepeat = 12, center = FALSE)
-  eb <- esaBcv::EsaBcv(Y = m, niter = niter, r.limit = maxPCs, nRepeat = holdoutRepeat, center = center, only.r = TRUE)
-  eb$result.list 
+rcvs <- function(m, maxPCs = 5, holdoutRepeat = 100, niter = 1, center = FALSE) {
+   
 }
 
-bcv <- function(m, maxPCs, repetition = 20, center = TRUE) {
-  # ps <- unlist(apply(rcvs, MARGIN = 1, function(row) {
-  #   
-  #   min(which(row < 0)) - 1
-  #   which.min(row)
-  # }))
-  
-  ebTest <- esaBcv::EsaBcv(Y = m, r.limit = 3, nRepeat = NULL)
-  k <- min(nrow(ebTest$result.list), ncol(m), 10)
-  
-  ps <- unlist(sapply(seq_len(repetition), function(i) {
+esabcvWrapper <- function(m, maxPCs, holdoutRep = 20, niter = 1, center = TRUE) {
+  res <- unlist(sapply(seq_len(repetition), function(x) {
     # takes the argmin[index] of the colmeans of BCV prediction error
-    results <- rcvs(m, maxPCs, holdoutRepeat = k^2, center = center)
-    as.numeric(names(which.min(colMeans(results))))
+    eb <- NULL
+    while(is.null(eb)) {
+      try(
+        eb <- esaBcv::EsaBcv(Y = m, niter = niter, r.limit = maxPCs, nRepeat = holdoutRep, center = center, only.r = TRUE)
+      )
+    }
+    
+    as.numeric(names(which.min(eb$result.list[1, ])))
   }))
-  ps
+  
+  list(best = mean(res), results = res)
 }
 
 evalEsaBcv.sim <- function(numBiclust = NULL, maxPCs = 10, center = TRUE, noise = 0.25, save = FALSE) {
@@ -75,31 +71,27 @@ testCenter <- function(input) {
   genData3 <- genSimData(3,0.01)
   simData3 <- genSimData3()
   
-  uncentered <- sapply(1:1000, FUN = function(x) {
+  iter1 <- sapply(1:1000, FUN = function(x) {
     sd3 <- tryCatch(esaBcv::EsaBcv(simData3, center = FALSE, niter = 1, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
                     error = function(e) "error")
     gd3 <- tryCatch(esaBcv::EsaBcv(genData3, center = FALSE, niter = 1, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
                     error = function(e) "error")
-    i <- tryCatch(esaBcv::EsaBcv(input, center = FALSE, niter = 1, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
-                  error = function(e) "error")
-    c(sd3, gd3, i)
+    c(sd3, gd3)
   })
   
-  centered <- sapply(1:1000, FUN = function(x) {
-    sd3 <- tryCatch(esaBcv::EsaBcv(simData3, center = TRUE, niter = 1, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
+  iter3 <- sapply(1:1000, FUN = function(x) {
+    sd3 <- tryCatch(esaBcv::EsaBcv(simData3, center = FALSE, niter = 3, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
                     error = function(e) "error")
-    gd3 <- tryCatch(esaBcv::EsaBcv(simData3, center = TRUE, niter = 1, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
+    gd3 <- tryCatch(esaBcv::EsaBcv(simData3, center = FALSE, niter = 3, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
                     error = function(e) "error")
-    i <- tryCatch(esaBcv::EsaBcv(simData3, center = TRUE, niter = 1, r.limit = 6, nRepeat = 2, only.r = TRUE)$best.r,
-                  error = function(e) "error")
-    c(sd3, gd3, i)
+    c(sd3, gd3)
   })
   
-  uncentered <- t(uncentered)
-  colnames(uncentered) <- c("simdata3", "gendata3", "simdata5")
-  apply(uncentered, MARGIN = 2, table)
+  iter1 <- t(iter1)
+  colnames(iter1) <- c("simdata3", "gendata3")
+  apply(iter1, MARGIN = 2, table)
   
-  centered <- t(centered)
-  colnames(centered) <- c("simdata3", "gendata3", "simdata5")
-  apply(centered, MARGIN = 2, table)
+  iter3 <- t(iter3)
+  colnames(iter3) <- c("simdata3", "gendata3")
+  apply(iter3, MARGIN = 2, table)
 }
