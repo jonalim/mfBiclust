f922db1ba3818ff68caac05a41e9a6007512f033
Jonathan Lim <2329118L@student.gla.ac.uk>
2018-07-17 17:12:44 +0100

    Conform to R package data conventions
    Former-commit-id: 1acd1979e9e560a3ab2f2ac2ab3fcd93649b0cc2

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Gene expression datasets for benchmarking classification performance
+#'
+#' Microarray and RNAseq datasets from biclustlib, originally published by
+#' de Souto MC, Costa IG, de Araujo DS, Ludermir TB, Schliep A in 2008.
+#'
+#' @format A list of 13 datasets where each element contains:
+#' \describe{
+#'   \item{data}{a dataframe of expression values, unmodified from biclustlib}
+#'   \item{labels}{a binary matrix where rows are pre-classified samples and
+#'     columns are named cancer categories}
+#' }
+#' @source \url{https://github.com/padilha/biclustlib/tree/master/}
+"cancer_benchmark"
+
+#' Gene expression datasets for benchmarking detection of biological signal
+#'
+#' A dataset containing the microarray datasets in biclustlib originally
+#' published by Jaskowiak PA, Campello RJ, Costa Filho IG (2013).
+#'
+#' @format A list of 17 data.frames where columns are samples and rows are
+#'   genes, with ENSEMBL names
+#'
+#' @source \url{https://github.com/padilha/biclustlib/tree/master/}
+#' 
+"yeast_benchmark"
