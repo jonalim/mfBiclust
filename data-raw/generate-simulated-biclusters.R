409117ce5ed6121ab6365307a0126711432ff3f4
Jonathan Lim <2329118L@student.gla.ac.uk>
2018-07-27 14:04:13 +0100

    ALS-NMF recover silently from stochastic fcnnls errors. Allow up to 9 restarts of any kind.
    Plaid also increase shuffle automatically to get the desired number of biclusters.
    
    If verbose, return details on the final helper call used to compute biclusters.
    
    
    Former-commit-id: c4fb60d689db02133e112c660bceeedf98450238

--- data-raw/generate-simulated-biclusters.R
+++ data-raw/generate-simulated-biclusters.R
@@ -1,24 +1,25 @@
 set.seed(12345)
-nonoverlap_3biclusters <- genSimData(n = 3, overlapped = FALSE, noise = 0.01,
+nonoverlap_3biclusters <- genSimData(n = 3, overlapped = FALSE, noise = 0.03,
                                      dynamSize = FALSE, dimx = 40)
-nonoverlap_15biclusters <- genSimData(n = 15, overlapped = FALSE, noise = 0.01,
+nonoverlap_15biclusters <- genSimData(n = 15, overlapped = FALSE, noise = 0.03,
                                       dynamSize = FALSE, dimx = 40)
 
-nonoverlap_5height <- genSimData(n= 3, overlapped = FALSE, noise = 0.01,
-                               dynamSize = FALSE, dimx = 200, dimy = 40)
+nonoverlap_10height <- genSimData(n= 3, overlapped = FALSE, noise = 0.03,
+                               dynamSize = FALSE, dimx = 400, dimy = 50)
 
-nonoverlap_5width <- genSimData(n= 3, overlapped = FALSE, noise = 0.01,
-                                 dynamSize = FALSE, dimx = 40, dimy = 200)
+nonoverlap_10width <- genSimData(n= 3, overlapped = FALSE, noise = 0.03,
+                                 dynamSize = FALSE, dimx = 50, dimy = 400)
 
-overlap_3biclusters <- genSimData(n = 3, overlapped = TRUE, noise = 0.01,
+overlap_3biclusters <- genSimData(n = 3, overlapped = TRUE, noise = 0.03,
                                   dynamSize = FALSE, dimx = 40)
 set.seed(123)
-overlap_4biclusters <- genSimData(n = 4, overlapped = TRUE, noise = 0.01,
+overlap_4biclusters <- genSimData(n = 4, overlapped = TRUE, noise = 0.03,
                                   dynamSize = FALSE, dimx = 40)
 
-simdata <- list(nonoverlap_3biclusters, nonoverlap_15biclusters, nonoverlap_5height,
-                nonoverlap_5width, overlap_3biclusters, overlap_4biclusters)
+simdata <- list(nonoverlap_3biclusters, nonoverlap_15biclusters,
+                nonoverlap_10height, nonoverlap_10width, overlap_3biclusters,
+                overlap_4biclusters)
 names(simdata) <- c("nonoverlap_3biclusters", "nonoverlap_15biclusters",
-                    "nonoverlap_5height", "nonoverlap_5width",
-                    "overlap_3biclusters", "overlap_4biclusters")
+                    "nonoverlap_10height", "nonoverlap_10width", "overlap_3biclusters",
+                    "overlap_4biclusters")
 usethis::use_data(simdata, overwrite = TRUE)
